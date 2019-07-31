7d307bb5505c40cba4c8b03fe834cb1faa1e18e1
kknudso1 <kyle.knudson@optum.com>
2019-07-31 14:45:53 -0500

    genetic Algorithm for Cube.

--- geneticAlgorithm.py
+++ geneticAlgorithm.py
@@ -9,7 +9,9 @@ mutation
 
 import random
 from rubixCube import RubixCube
+import copy
 
+rc = RubixCube()
 
 def initialize_pop(population_size, chromosome_size):
     population = []
@@ -19,75 +21,75 @@ def initialize_pop(population_size, chromosome_size):
 
 
 def create_chromosome(size):
-    possible_moves = ['backco','back', 'forwardco','forward', 'topco','top', 'bottomco','bottom', 'leftco','left', 'rightco','right', 'nothing']
+    possible_moves = ['backco','back', 'forwardco','forward', 'topco','top', 'bottomco','bottom', 'leftco','left', 'rightco','right', 'noMove']
     chromosome = []
     for _ in range(size):
-        index = random.randint(0, 12)
+        index = random.randint(0, 11)
         chromosome.append(possible_moves[index])
     return chromosome
 
 
 def fitness(chromosome):
-    rubixCube = RubixCube()
+    cube = copy.deepcopy(rc)
     for move in chromosome:
         if move == 'backco':
-            rubixCube.backRotate(True)
+            cube.backRotate(True)
         elif move == 'back':
-            rubixCube.backRotate(False)
+            cube.backRotate(False)
         elif move == 'forwardco':
-            rubixCube.frontRotate(True)
+            cube.frontRotate(True)
         elif move == 'forward':
-            rubixCube.frontRotate(False)
+            cube.frontRotate(False)
         elif move == 'topco':
-            rubixCube.topRotate(True)
+            cube.topRotate(True)
         elif move == 'top':
-            rubixCube.topRotate(False)
+            cube.topRotate(False)
         elif move == 'bottomco':
-            rubixCube.bottomRotate(True)
+            cube.bottomRotate(True)
         elif move == 'bottom':
-            rubixCube.bottomRotate(False)
+            cube.bottomRotate(False)
         elif move == 'leftco':
-            rubixCube.leftRotate(True)
+            cube.leftRotate(True)
         elif move == 'left':
-            rubixCube.leftRotate(False)
+            cube.leftRotate(False)
         elif move == 'rightco':
-            rubixCube.rightRotate(True)
+            cube.rightRotate(True)
         elif move == 'right':
-            rubixCube.rightRotate(False)
-    totalScore = 0
-    for i in range(6):
-        totalScore += rubixCube.getScore(i)
-    return totalScore
-
+            cube.rightRotate(False)
+        else:
+            pass
+    score = cube.getScore(0) + cube.getScore(1) + cube.getScore(2) + cube.getScore(3) + cube.getScore(4) + cube.getScore(5)
+    return score
 
 def crossover(chrom1, chrom2):
-    index = random.randint(0, 5)
+    index = random.randint(1, 4)
     chrom1slice = chrom1[:index]
     chrom2slice = chrom2[:index]
 
-    # print('Initial Chromosome')
-    # print(chrom1)
-    # print(chrom2)
+    #print('Initial Chromosome')
+    #print(chrom1)
+    #print(chrom2)
 
-    # print('slices')
-    # print(chrom1slice)
-    # print(chrom2slice)
+    #print('slices')
+    #print(chrom1slice)
+    #print(chrom2slice)
 
     chrom1[:index] = chrom2slice
     chrom2[:index] = chrom1slice
 
-    # print('Chromosomes after crossover')
-    # print(chrom1)
-    # print(chrom2)
+    #print('Chromosomes after crossover')
+    #print(chrom1)
+    #print(chrom2)
 
     return chrom1, chrom2
 
 
 def mutation(chrom):
-    possible_moves = ['backco','back', 'forwardco','forward', 'topco','top', 'bottomco','bottom', 'leftco','left', 'rightco','right', 'nothing']
-    move_index = random.randint(0, 12)
+    possible_moves = ['backco','back', 'forwardco','forward', 'topco','top', 'bottomco','bottom', 'leftco','left', 'rightco','right', 'nomove']
+    move_index = random.randint(0, 11)
     index = random.randint(0,len(chrom)-1)
     chrom[index] = possible_moves[move_index]
+    print('Mutation occured!')
     return chrom
 
 
@@ -113,25 +115,31 @@ def find_lowest(population):
     return lowest, lowest_index
 
 
-def simulation(generation_size, pop_size, chrom_size):
+def simulation(generation_size, pop_size, chrom_size, mutation_rate, max_pop):
     population = initialize_pop(population_size=pop_size, chromosome_size=chrom_size)
     while generation_size != 0:
         rand_index = random.randint(0, pop_size - 1)
         top_chromosomes = selection(population)
-        chrom1 = top_chromosomes[0]
-        chrom2 = top_chromosomes[1]
+        chrom1 = list.copy(top_chromosomes[0])
+        chrom2 = list.copy(population[find_lowest(population)[1]])
         new_chromosomes = crossover(chrom1, chrom2)
         for index, chromosome in enumerate(new_chromosomes, start=0):
-            population[find_lowest(population)[1]] = new_chromosomes[index]
-        population[rand_index] = mutation(population[rand_index])
-        print('Top Chromosome: ', chrom1)
-        print('fitness Score: ', fitness(chrom1))
+            population.append(chromosome)
+        if len(population)>max_pop:
+            for _ in range(5):
+                del population[find_lowest(population)[1]]
+                print('Removing chromosome:', population[find_lowest(population)[1]])
+        if random.random() < mutation_rate:
+            population[rand_index] = mutation(population[rand_index])
+        top = selection(population)[0]
+        print('Top Chromosome: ', top)
+        print('fitness Score: ', fitness(top))
         generation_size -= 1
 
 
 
 if __name__ == '__main__':
-    simulation(500, 25, 4)
+    simulation(50, 10, 6, 0.5, 50)
 
 
 
